2e4981691de256d99744e76a44c5c30afe7669ad
Elvin Zhang <zhangweicalm@gmail.com>
2024-09-16 15:07:34 -0400

    test wechat share image 1

--- components/ShareButton.tsx
+++ components/ShareButton.tsx
@@ -13,7 +13,6 @@ interface ShareButtonProps {
 
 export default function ShareButton({ title, text, url, cardId }: ShareButtonProps) {
   const [isShared, setIsShared] = useState(false);
-console.log(title, text, url, cardId)
   const captureAndShare = async () => {
     const cardElement = document.getElementById(cardId);
     if (!cardElement) {
@@ -27,26 +26,26 @@ console.log(title, text, url, cardId)
         canvas.toBlob((blob) => resolve(blob!), 'image/png')
       );
 
-    //   const filesArray = [
-    //     new File([imageBlob], 'ride_details.png', { type: 'image/png' })
-    //   ];
+       const filesArray = [
+        new File([imageBlob], 'ride_details.png', { type: 'image/png' })
+      ];
 
-    //   if (navigator.share && navigator.canShare({ files: filesArray })) {
-    //     await navigator.share({
-    //       title,
-    //       text,
-    //       url,
-    //       files: filesArray,
-    //     });
-    //     setIsShared(true);
-    //     setTimeout(() => setIsShared(false), 2000);
-    //   } else {
+      if (navigator.share && navigator.canShare({ files: filesArray })) {
+        await navigator.share({
+          title,
+          text,
+          url,
+          files: filesArray,
+        });
+        setIsShared(true);
+        setTimeout(() => setIsShared(false), 2000);
+      } else {
         // Fallback for browsers that don't support sharing files
         const imageUrl = URL.createObjectURL(imageBlob);
         window.open(imageUrl, '_blank');
         setIsShared(true);
         setTimeout(() => setIsShared(false), 2000);
-     // }
+      }
     } catch (err) {
       console.error('Error capturing or sharing:', err);
     }
