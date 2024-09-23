768f6b64a5721c794911db65c6ebd0a22ad78e22
Elvin Zhang <zhangweicalm@gmail.com>
2024-09-22 22:41:35 -0400

    09222241

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -10,7 +10,7 @@ export default function Modal({ isOpen, onClose, children }: ModalProps) {
     if (!isOpen) return null;
 
     return (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 p-4">
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-100 p-4">
             <div className="bg-white rounded-lg shadow-xl max-w-md w-full max-h-[90vh] flex flex-col relative">
                 <button 
                     onClick={onClose} 
